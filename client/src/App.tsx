a8c2f9eaafb52fa4c0c903087eaaf9d4c257f56b
Haruto Kusaka <haruto.k.4230@gmail.com>
2024-06-08 16:59:11 +0900

    style : 中央揃えにスタイル変更

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,17 @@
-import { Container, Stack } from '@chakra-ui/react'
-import { TodoForm, TodoList } from './components'
+import { Center, Container } from '@chakra-ui/react';
+import { TodoLayout, TodoForm, TodoList } from './components'
+
+export const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api";
 
 function App() {
-
+  
   return (
-    <Stack h="100vh">
-      <Container >
-        <TodoForm />
-        <TodoList />
-      </Container>
-    </Stack>
+    <Center w="100vw" h="100vh">
+      <TodoLayout>
+          <TodoForm />
+          <TodoList />    
+      </TodoLayout>      
+    </Center>
   )
 }
 

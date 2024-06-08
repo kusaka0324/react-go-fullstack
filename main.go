0c0bf358f066e160c074733fe94fcfbfce20689a
Haruto Kusaka <haruto.k.4230@gmail.com>
2024-06-08 16:13:40 +0900

    delete : corsの設定削除

--- main.go
+++ main.go
@@ -54,11 +54,6 @@ func main() {
 
 	app := fiber.New()
 
-	// app.Use(cors.New(cors.Config{
-	// 	AllowOrigins: "http://localhost:5173",
-	// 	AllowHeaders: "Origin,Content-Type,Accept",
-	// }))
-
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
 	app.Patch("/api/todos/:id", updateTodo)
